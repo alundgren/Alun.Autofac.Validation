328756fd4047948db66787c8bad9c11eae8a77b4
Andreas Lundgren <andreas.lundgren@gmail.com>
2011-09-15 22:05:13 +0200

    First version

---
+++ Alun.AutoFac.Validation/AutofacExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Autofac.Core;
+
+namespace Autofac
+{
+    public static class AutofacExtensions
+    {
+        /// <summary>
+        /// Lops  through all the services
+        /// and tries to instantiate them. Any problems will be reported using assertFail.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <param name="assertFail">called when problems are found</param>
+        public static void AssertConfigurationIsValid(this IContainer container, Action<string> assertFail)
+        {
+            var problems = FindConfigurationProblems(container);
+            if(problems.Count > 0)
+                assertFail(
+                    string.Join(
+                    Environment.NewLine, 
+                    problems.Select(ex => ex.Message).ToArray()));
+        }
+
+        /// <summary>
+        /// Calls the other version of DebugAssertConfigurationIsValid using
+        /// Debug.Assert as assertFail
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public static void AssertConfigurationIsValid(this IContainer container)
+        {
+            AssertConfigurationIsValid(container, s => Debug.Assert(false, s));
+        }
+
+        /// <summary>
+        /// Tries to find any configuration problems
+        /// by going through each of the services registered and trying to resolve them. 
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <returns>A list of exceptions from resolve attemps or an empty list if no problems were found.</returns>
+        public static IList<Exception> FindConfigurationProblems(this IContainer container)
+        {
+            var services = container
+                .ComponentRegistry
+                .Registrations
+                .SelectMany(x => x.Services)
+                .OfType<TypedService>()
+                .Where(x => !x.ServiceType.Name.StartsWith("Autofac"))
+                .ToList();
+            var exceptions = new List<Exception>();
+            foreach (var typedService in services)
+            {
+                try
+                {
+                    container.Resolve(typedService.ServiceType);
+                }
+                catch (DependencyResolutionException ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            return exceptions;
+        }
+    }
+}
---
+++ Alun.Autofac.Validation.Test/ValidationTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+using Bar;
+using Foo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foo
+{
+    public class MyItem
+    {
+        public string Name { get; set; }
+    }
+    public interface IRepository
+    {
+        MyItem GetItem();
+    }
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
+namespace Bar
+{
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MyService
+    {
+        private readonly Logger _logger;
+
+        public MyService(Logger logger)
+        {
+            _logger = logger;
+        }
+    }
+    public class Repository : IRepository
+    {
+        private readonly ILogger _logger;
+
+        public Repository(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public MyItem GetItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+namespace Alun.Autofac.Validation.Test
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void DemonstrateIntenedUsage()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Repository>().As<IRepository>();
+            builder.RegisterType<MyService>();
+            var container = builder.Build();
+
+            container.AssertConfigurationIsValid(Assert.Fail);
+        }
+
+        [TestMethod]
+        public void FindsMissingInterfaceDependancy()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Repository>().As<IRepository>();
+            var container = builder.Build();
+
+            var result = container.FindConfigurationProblems();
+            Assert.IsTrue(result[0].Message.Contains("None of the constructors found with 'Public binding flags' on type 'Bar.Repository' can be invoked with the available services and parameters"), result[0].Message);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void FindsMissingConcreteDependancy()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MyService>();
+            var container = builder.Build();
+
+            var result = container.FindConfigurationProblems();
+            Assert.IsTrue(result[0].Message.Contains("None of the constructors found with 'Public binding flags' on type 'Bar.MyService' can be invoked with the available services and parameters"), result[0].Message);
+            Assert.AreEqual(1, result.Count);            
+        }
+    }
+}
